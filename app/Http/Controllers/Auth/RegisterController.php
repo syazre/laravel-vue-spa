b637049dcd3d3bff664c2046f0379df7a94e98f0
Syafiq Syazre <syfqsyzre@gmail.com>
2021-09-30 18:47:33 +0800

    Initial commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Notifications\Notifiable;
+use App\Notifications\SetPassword;
+
+use Illuminate\Support\Facades\Password;
+
+class RegisterController extends Controller
+{
+    use RegistersUsers, SendsPasswordResetEmails, Notifiable;
+
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * The user has been registered.
+     */
+    protected function registered(Request $request, User $user)
+    {
+        if ($user instanceof MustVerifyEmail) {
+            return response()->json(['status' => trans('verification.sent')]);
+        }
+        $token = Password::broker()->createToken($user);
+
+        if($request) {
+            $user->notify(new SetPassword($token));
+        }
+
+        return response()->json($user);
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'email' => 'required|email:filter|max:255|unique:users',
+            // 'password' => 'required|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     */
+    protected function create(array $data): User
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            // 'password' => bcrypt($data['password']),
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+
+class ResetPasswordController extends Controller
+{
+    use ResetsPasswords;
+
+    protected $redirectTo = '/login';
+
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get the response for a successful password reset.
+     */
+    protected function sendResetResponse(Request $request, string $response)
+    {
+        return ['status' => trans($response)];
+    }
+
+    /**
+     * Get the response for a failed password reset.
+     */
+    protected function sendResetFailedResponse(Request $request, string $response)
+    {
+        return response()->json(['email' => trans($response)], 400);
+    }
+}
